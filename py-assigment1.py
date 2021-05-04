a2fb9cedb8b2a038b3937349ab86b8af5d60435d
Kenneth Adjei <[kmadjei25@gmail.com]>
2021-05-04 09:41:28 -0400

    Added function for getting user data for list of courses

--- py-assigment1.py
+++ py-assigment1.py
@@ -1,9 +1,19 @@
 def get_course_marks():
     """
-    Get the number of courses user has completed
+    Get the details of the courses the user has completed and validate the data.
     """
 
-    num_of_courses = input("Please enter the:  ")
+    while True:
+        try:
+            num_of_courses = int(input("Please enter the number of courses you are taking:  "))
+            if num_of_courses < 0 :
+                raise TypeError(f"{num_of_courses} cannot be below 0")
+            else:
+                break   
+        except TypeError as e:
+            print(f"Invalid data: {e}. Please enter an actual number.\n")
+        except ValueError:
+            print(f"Invalid entry: {num_of_courses}. Please provide an integer number.\n")
 
     course_marks = {}
 
@@ -21,3 +31,7 @@ def get_course_marks():
                 print(f"Invalid data: {mark}. Please enter an actual number.\n")
             except ValueError:
                 print(f"You entered: {mark}. Your marks cannot be negative. Please Try again.\n")
+
+    print("Your marks are:", course_marks)
+
+get_course_marks()
