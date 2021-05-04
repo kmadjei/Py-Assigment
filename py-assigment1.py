40b8f6f00423f2afed0f64cb65e4cddfd57784df
Kenneth Adjei <[kmadjei25@gmail.com]>
2021-05-04 10:32:51 -0400

    Calculate the grade and created the function to execute the program

--- py-assigment1.py
+++ py-assigment1.py
@@ -1,3 +1,5 @@
+from statistics import mean
+
 def get_course_marks():
     """
     Get the details of the courses the user has completed and validate the data.
@@ -32,6 +34,36 @@ def get_course_marks():
             except ValueError:
                 print(f"You entered: {mark}. Your marks cannot be negative. Please Try again.\n")
 
-    print("Your marks are:", course_marks)
+    print(f"\nYour marks are: {course_marks}.\n" )
+    return course_marks
 
-get_course_marks()
+def calculate_grade(data):
+    """
+    Find the average marks and issue a grade
+    """
+    print("Calculating your grades...\n")
+
+    avg = round(mean(data.values()), 2)
+
+    print((f"The average for your {len(data)} course(s) is: {avg}."))
+    
+    if avg >= 90 and avg <= 100:
+        print("Your grade is A+")
+    elif avg >= 80 and avg <= 89:
+        print("Your grade is B")
+    elif avg >= 70 and avg <= 79:
+        print("Your grade is C")
+    elif avg >= 60 and avg <= 69:
+        print("Your grade is D")
+    else:
+        print("Your grade is F")
+
+def main():
+    """
+    Runs all the function for the program
+    """
+    course_data = get_course_marks()
+    calculate_grade(course_data)
+    print("\n")
+
+main()
